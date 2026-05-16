dbb36c6fbdbe8e1c1f62a07004fbf81548fc898f
Mushvig Shukurov <musfiqsukurov9@gmail.com>
2026-05-16 17:30:53 +0400

    Day-10 url slashlari

--- contact/urls.py
+++ contact/urls.py
@@ -4,9 +4,9 @@ from contact.views import contact_view, messages_view, message_read_view, messag
 
 urlpatterns = [ 
     path('', contact_view, name="contact"),
-    path('/messages/', messages_view, name="contactmessages"),
-    path('/messages/detail/<int:id>', message_detail_view, name="contactmessagedetail"),
-    path('/messages/delete', delete_contact_message_view, name="deletecontactmessage"),
-    path('/messages/change-read/<int:id>', message_read_view, name="changemessageisread"),
-    path('/messages/change-public/<int:id>', message_public_view, name="changemessageispublic"),
+    path('messages/', messages_view, name="contactmessages"),
+    path('messages/detail/<int:id>', message_detail_view, name="contactmessagedetail"),
+    path('messages/delete', delete_contact_message_view, name="deletecontactmessage"),
+    path('messages/change-read/<int:id>', message_read_view, name="changemessageisread"),
+    path('messages/change-public/<int:id>', message_public_view, name="changemessageispublic"),
 ]
--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include("member.urls")),
-    path('contact', include("contact.urls")),
+    path('contact/', include("contact.urls")),
     path('dashboard/', include("dashboard.urls")),
 ]
