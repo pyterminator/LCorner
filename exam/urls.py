eefd524c5d516a6b7eaa1a26c3bf3bbd64462293
Mushvig Shukurov <musfiqsukurov9@gmail.com>
2026-06-27 13:44:48 +0400

    Day-52 2

--- exam/urls.py
+++ exam/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path 
-from exam.views import MyExams, CreateNewExam
+from exam.views import MyExams, CreateNewExam, SentenceBuilder
 
 
 urlpatterns = [
     path('of-mine', MyExams, name="myexams"),
     path('create-new', CreateNewExam, name="createnewexam"),
+    path("system-sentence-builder-game", SentenceBuilder, name="sentencebuilder"),
 ]
--- exam/views.py
+++ exam/views.py
@@ -9,3 +9,15 @@ def MyExams(request):
 @user_passes_test(lambda u: u.is_staff)
 def CreateNewExam(request):
     return render(request, "exam/create-new-exam.html") 
+
+
+@login_required
+def SentenceBuilder(request):
+
+    data = {
+        "words":['is', 'what', 'name', 'your'],
+        "title":"Sözləri düzgün ardıcıllıqla yerləşdir",
+        "answer":"what is your name"
+    }
+
+    return render(request, "sentence-builder.html", context=data)
