44f01d1be45e09f9c658f3d3d4091a2f5ebe5004
Mushvig X1 <musfiqsukurov9@gmail.com>
2026-08-06 10:48:22 +0400

    Day 92 10:48

--- exam/urls.py
+++ exam/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('create-new', CreateNewExam, name="createnewexam"),
     path('update/<str:slug>', UpdateExam, name="updateexam"),
     path('exam-detail/<str:slug>', ExamDetailView, name="examdetail"),
+    path('exam-pano/checkanswer', CheckAnswer, name='exampanocheckanswer'),
     path('exam-pano/<str:slug>', ExamPano, name="exampano"),
     path('generate-quiz-for-exam-pano/<str:slug>', GenerateQuizForExamPano, name="generatequizforexampano"),
     path('activate-exam/<int:id>', ActivateExam, name="activateexam"),
